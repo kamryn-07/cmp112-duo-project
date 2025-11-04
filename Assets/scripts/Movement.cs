52fe746f3875e196ee4488f9d890499f0ca2e409
Kamryn <kamrynrobb07@gmail.com>
2025-11-04 15:23:53 +0000

    Movement Tweaking and (first) Utility Addition
    Added a 'dash' mechanic to the movement script with a cooldown and some variable values, whilst slightly changing some of the other movement code and cleaning up the script slightly

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Diagnostics;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,21 +9,26 @@ public class Movement : MonoBehaviour
 
     InputAction moveAction;
     InputAction jumpAction;
+    InputAction utilityAction;
     GameObject player;
     Rigidbody playerRigidBody;
 
     const float SPEED = 4.5f;
-    const float ACCELERATION = SPEED / 5;
     const float JUMP_FORCE = 100.0f;
+    const float UTILITY_FORCE = 2000.0f;
+    const float UTILITY_COOLDOWN = 1.5f;
     const float DOWNWARD_FORCE = -2.0f;
     float xSpeed = 0.0f;
     float ySpeed = 0.0f;
+
+    bool utilityDebounce = false;
 
     void Start()
     {
 
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        utilityAction = InputSystem.actions.FindAction("Utility");
         player = GameObject.Find("Player");
         playerRigidBody = player.GetComponent<Rigidbody>();
 
@@ -38,9 +45,16 @@ public class Movement : MonoBehaviour
         // player movement handling and increased gravity (downward force applied)
         playerRigidBody.AddForce(xSpeed, DOWNWARD_FORCE, ySpeed);
 
+        // pressed actions
         if (jumpAction.IsPressed() && IsGrounded())
         {
             playerRigidBody.AddForce(0, JUMP_FORCE, 0);
+        }
+        if (utilityAction.IsPressed() && !utilityDebounce)
+        {
+            UnityEngine.Debug.Log("activated");
+            StartCoroutine(activateUtility(moveValue));
+            UnityEngine.Debug.Log("off cooldown");
         }
 
     }
@@ -48,8 +62,21 @@ public class Movement : MonoBehaviour
     // raycast to find if player is grounded for jump so u can't inf jump
     public bool IsGrounded()
     {
+
         float distanceToGround = GetComponent<Collider>().bounds.extents.y;
         return Physics.Raycast(transform.position, Vector3.down, distanceToGround + 0.1f);
+
+    }
+    IEnumerator activateUtility(Vector2 v2MoveValue)
+    {
+
+        utilityDebounce = true;
+        Vector2 direction = v2MoveValue;
+        Vector3 force = new(UTILITY_FORCE * direction.x, 0, UTILITY_FORCE * direction.y);
+        playerRigidBody.AddForce(force);
+        yield return new WaitForSeconds(UTILITY_COOLDOWN);
+        utilityDebounce = false;
+
     }
 
 }

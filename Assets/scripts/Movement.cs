6f67f8f16aeea6d6c2cdb880d69d5813388043ef
Kamryn <kamrynrobb07@gmail.com>
2025-11-09 09:08:47 +0000

    Walljump Implementation
    I have no doubts this will have bugs which will have to be ironed out in the future, but for now this works pretty damn well so it's being committed, I DON'T CARE!!!!!!

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -18,6 +18,8 @@ public class Movement : MonoBehaviour
     public float UTILITY_FORCE = 3500.0f;
     public float UTILITY_COOLDOWN = 1.5f;
     public float DOWNWARD_FORCE = -50.0f;
+    public float WALLJUMP_THRESHOLD = 50.0f;
+    public float WALLJUMP_VERTICAL_DIVISION = 2.5f;
 
     InputAction moveAction;
     InputAction jumpAction;
@@ -28,9 +30,11 @@ public class Movement : MonoBehaviour
     public float xSpeed = 0.0f;
     public float ySpeed = 0.0f;
 
+    private RaycastHit[] hits;
+
     bool utilityDebounce = false;
 
-    void Start()
+    private void Start()
     {
 
         moveAction = InputSystem.actions.FindAction("Move");
@@ -59,14 +63,35 @@ public class Movement : MonoBehaviour
         playerRigidBody.AddRelativeForce(xSpeed, 0, ySpeed);
 
         // pressed actions & gravity application
-        if (IsGrounded())
+        if (IsGrounded() && jumpAction.IsPressed())
         {
-            if (jumpAction.IsPressed())
-            {
-                playerRigidBody.AddForce(0, JUMP_FORCE, 0);
-            }
+            OnJump();
         }
-        else
+        else if (!IsGrounded() && jumpAction.IsPressed())
+        {
+
+            hits = new RaycastHit[4];
+            hits = GetWallHits();
+
+            if (hits[0].normal != Vector3.zero)
+            {
+                OnWallJump(hits[0].normal);
+            }
+            else if (hits[1].normal != Vector3.zero)
+            {
+                OnWallJump(hits[1].normal);
+            }
+            else if (hits[2].normal != Vector3.zero)
+            {
+                OnWallJump(hits[2].normal);
+            }
+            else if (hits[3].normal != Vector3.zero)
+            {
+                OnWallJump(hits[3].normal);
+            }
+
+        }
+        if (!IsGrounded())
         {
             playerRigidBody.AddForce(0, DOWNWARD_FORCE, 0);
         }
@@ -78,13 +103,27 @@ public class Movement : MonoBehaviour
     }
 
     // raycast to find if player is grounded for jump so u can't inf jump
-    public bool IsGrounded()
+    private bool IsGrounded()
     {
 
         float distanceToGround = GetComponent<Collider>().bounds.extents.y;
         return Physics.Raycast(transform.position, Vector3.down, distanceToGround + 0.1f);
 
     }
+
+    // specifically for wall jumps, of course
+    private RaycastHit[] GetWallHits()
+    {
+
+        Physics.Raycast(transform.position, Vector3.left, out var hitL, WALLJUMP_THRESHOLD);
+        Physics.Raycast(transform.position, Vector3.right, out var hitR, WALLJUMP_THRESHOLD);
+        Physics.Raycast(transform.position, Vector3.forward, out var hitF, WALLJUMP_THRESHOLD);
+        Physics.Raycast(transform.position, Vector3.back, out var hitB, WALLJUMP_THRESHOLD);
+
+        return new RaycastHit[4]{ hitL, hitR, hitF, hitB };
+
+    }
+
     IEnumerator ActivateUtility(Vector2 v2MoveValue)
     {
 
@@ -95,6 +134,19 @@ public class Movement : MonoBehaviour
         cooldownBarScript.StartCoroutine("InitiateCooldown", UTILITY_COOLDOWN);
         yield return new WaitForSecondsRealtime(UTILITY_COOLDOWN);
         utilityDebounce = false;
+
+    }
+    private void OnJump()
+    {
+
+        playerRigidBody.AddForce(0, JUMP_FORCE, 0);
+
+    }
+    private void OnWallJump(Vector3 normal)
+    {
+
+        UnityEngine.Debug.Log(normal);
+        playerRigidBody.AddForce(JUMP_FORCE * normal.x, JUMP_FORCE / WALLJUMP_VERTICAL_DIVISION, JUMP_FORCE * normal.z);
 
     }
 

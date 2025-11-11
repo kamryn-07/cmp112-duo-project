c8d6aef394b5ccd2b4d331adb077abfdbbc793f2
Kamryn <kamrynrobb07@gmail.com>
2025-11-11 03:31:46 +0000

    I DON'T REMEMNBVER

---
+++ Assets/Scripts/StartButtonController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonController : MonoBehaviour
+{
+
+    private string sceneName = "GameScene";
+
+    void OnClick()
+    {
+
+        UnityEngine.Debug.Log("hit");
+
+    }
+
+}
